2b6eed84cb8c41f166c8e916911ac9e8bc6e6e07
Mikhail Korolkevich <m.korolkevich@xsolla.com>
2021-02-05 23:14:47 +0500

    step five

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -4,6 +4,7 @@ import prompt
 
 def welcome_user():
     """Справшивает у пользователя его имя, а затем приветсвует его."""
-    name = prompt.string('May I have your name?')
+    name = prompt.string('May I have your name? ')
     text = 'Hello, {0}'.format(name)
     print(text)
+    return name
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -5,7 +5,8 @@ from brain_games import cli
 def main():
     """Привествует в игре, а затем делает персональное привествие."""
     print('Welcome to the Brain Games!')
-    cli.welcome_user()
+    name = cli.welcome_user()
+    return name
 
 
 if __name__ == '__main__':
