e8b5ccd0649f3ff9e5a00b91c85de09ea1b13d76
Mikhail Korolkevich <m.korolkevich@xsolla.com>
2021-02-13 22:18:13 +0500

    fix: refactoring logic

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -6,60 +6,40 @@ import prompt
 from brain_games.scripts import brain_games as sc
 
 
-def welcome():
-    """Привествует в игре, а затем объявляет правила игры.
+def is_prime(num):
+    """Функция определения правильных чисел.
 
+    Args:
+        num: Входное число для проверки.
     Returns:
-        Name user.
+        True | False для данного числа.
     """
-    name = sc.main()
-    print('Answer "yes" if given number is prime. Otherwise answer "no".')
-    return name
-
-
-def is_prime(n):
-    if n % 2 == 0:
-        return n == 2
-    d = 3
-    while d * d <= n and n % d != 0:
-        d += 2
-    return d * d > n
+    if num % 2 == 0:
+        return num == 2
+    divisor = 3
+    while divisor * divisor <= num and num % divisor != 0:
+        divisor += 2
+    return divisor * divisor > num
 
 
 def statement_generation():
-    start = randint(2, 2500)  # noqa:S311
-    is_prime(start)
-    correct_answer = 'yes' if is_prime(start) else 'no'
-    return start, correct_answer
+    """Функуия поределения условия задачи.
 
-
-def ask_question():
-    """Проверяет результаты ответов.
-
-    Returns:
-        Correct answer
-        User answer
+    Return:
+        question_arg: Random number.
+        correct_answer: True if prime else False.
     """
-
-    num, correct_answer = statement_generation()
-    print('Question: {0}'.format(num))
-    answer = prompt.string('Your answer: ')
-    return correct_answer, answer
+    end = 2500
+    question_arg = randint(2, end)  # noqa:S311
+    correct_answer = 'yes' if is_prime(question_arg) else 'no'
+    return question_arg, correct_answer
 
 
 def main():
     """Привествует в игре, а затем делает персональное привествие."""
-    name = welcome()
-    for attempt in range(0, 3):  # noqa:WPS122
-        answer, correct_answer = ask_question()
-        if answer == correct_answer:
-            print('Correct!')
-            if attempt == 2:
-                print('Congratulations, {0}!'.format(name))
-        else:
-            text = '"{0}" is wrong answer ;(. Correct answer was "{1}".'
-            print(text.format(answer, correct_answer))
-            break
+    rules = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+    name = sc.welcome(rules)
+    sc.game_flow(name, statement_generation)
 
 
 if __name__ == '__main__':
